5ca795672a35e270a5a58df3066fea1484e70895
felipesiqueira <B&atr1zG>
2018-12-27 23:49:53 -0200

    Refactory do ApeInfoHandler

--- src/main/java/br/com/frsiqueira/Commands.java
+++ src/main/java/br/com/frsiqueira/Commands.java
@@ -4,7 +4,7 @@ public class Commands {
     private static final String commandInitChar = "/";
     public static final String START = commandInitChar + "start";
     public static final String STOP = commandInitChar + "stop";
-    public static final String DAYS_REMAINING= commandInitChar + "dias-restantes";
-    public static final String ALERT = commandInitChar + "me-avise";
+    public static final String DAYS_REMAINING= "Dias Restantes";
+    public static final String ALERT = "Me avise quando tenho que pagar";
 
 }
